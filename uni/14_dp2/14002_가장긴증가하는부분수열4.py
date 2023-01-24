75fd7969f7eea888f80c2e6212886da00f08e328
janguni <lightson23@naver.com>
2023-01-24 19:41:12 +0900

    [BOJ] 가장 긴 증가하는 부분 수열4 / 골드4

--- uni/14_dp2/14002_가장긴증가하는부분수열4.py
+++ uni/14_dp2/14002_가장긴증가하는부분수열4.py
@@ -1,15 +1,22 @@
-arr1 = list(input())
-arr2 = list(input())
-n = len(arr1)
-m = len(arr2)
-dp = [[0] * (m+1) for _ in range(n+1)]
-print(dp)
+n = int(input())
+data = list(map(int, input().split()))
+dp = [1] * (n+1)
 
-k = 0
-for i in range(n+1):
-    for j in range(m+1):
-        print(i, j)
-        dp[i][j] = k
-        k += 1
+# LIS 길이 구하기
+for i in range(len(data)):
+    for j in range(i):
+        if data[j] < data[i]:
+            dp[i] = max(dp[i], dp[j]+1)
 
-print(dp)
+print(max(dp))
+x = max(dp)
+
+# LIS 구하기
+result = []
+for i in range(n-1, -1, -1):
+    if dp[i] == x:
+        result.append(data[i])
+        x -= 1
+result.reverse()
+for r in result:
+    print(r, end=' ')
