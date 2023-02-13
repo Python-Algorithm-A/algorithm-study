168601f57ae9ea3578bb71759c2376694d09b957
janguni <lightson23@naver.com>
2023-02-13 16:26:42 +0900

    [BOJ] 행렬곱셈순서4 / 실버1 / 10분

---
+++ uni/17_matrix/18236_행렬곱셈순서4.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+result=0
+last = matrix[n-1][1]
+for i in range(n-2, -1, -1):
+    result+=matrix[i][0] *matrix[i][1] * last
+print(result)
